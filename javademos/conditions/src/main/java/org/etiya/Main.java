5c8a1e7ecbf67c5c3c2dc84b6f17919e5782ca2c
serap.erkan <serap.erkannn@gmail.com>
2023-09-23 09:00:09 +0300

    conditions

---
+++ javademos/conditions/src/main/java/org/etiya/Main.java
@@ -0,0 +1,35 @@
+package org.etiya;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int number = 24;
+        if (number < 20) {
+            System.out.println("The number is less than 20.");
+        } else if (number == 20) {
+            System.out.println("The number is equal to 20");
+        } else if (number > 100) {
+            System.out.println("The number is greater than 10");
+        } else {
+            System.out.println("else");
+            //code to be executed if the condition is not met or is false
+        }
+    }
+
+}
+
+   /*
+   -->Conditions:
+   -if-else
+   -switch-case
+   -ternary
+
+   ***> Equality Comparison Operator:
+   *Equality Operator ==
+   *Inequality Operator !=
+   *Greater Than Operator >
+   *Less Than Operator <
+   *Greater Than or Equal To Operator >=
+   *Less Than or Equal To Operator <=
+   */
+
