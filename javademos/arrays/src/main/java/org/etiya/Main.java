e4285ffa794696b7ceb93089dfebd7b1d840b29f
serap.erkan <serap.erkannn@gmail.com>
2023-09-23 11:07:08 +0300

    multidimensional arrays

--- javademos/arrays/src/main/java/org/etiya/Main.java
+++ javademos/arrays/src/main/java/org/etiya/Main.java
@@ -25,5 +25,25 @@ public class Main {
         for (String array : arrays) {
             System.out.println(array);
         }
+
+        System.out.println("---------------");
+
+        String [][] cities=new String[3][3];
+        cities[0][0]="Istanbul";
+        cities[0][1]="Bursa";
+        cities[0][2]="Bilecik";
+        cities[1][0]="Ankara";
+        cities[1][1]="Konya";
+        cities[1][2]="Kayseri";
+        cities[2][0]="Diyarbakır";
+        cities[2][1]="Şanlıurfa";
+        cities[2][2]="Gaziantep";
+
+        for ( int i=0; i<3 ; i++){
+            for( int j=0;j<3;j++){
+                System.out.println(cities[i][j]);
+            }
+        }
+
     }
 }
