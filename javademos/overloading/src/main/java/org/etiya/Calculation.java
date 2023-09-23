886a50e9cb0eb1a8a5f4a33b94a42c904d64822f
serap.erkan <serap.erkannn@gmail.com>
2023-09-23 18:17:48 +0300

    overloading

---
+++ javademos/overloading/src/main/java/org/etiya/Calculation.java
@@ -0,0 +1,20 @@
+package org.etiya;
+
+public class Calculation {
+
+    public int total(int number1,int number2){
+        return number1+number2;
+    };
+    public int total(int number1,int number2,int number3){
+        return number1+number2+number3;
+    };
+
+
+  /*
+   --- Overloading
+           (aşırı yükleme veya aşırı yüklenme olarak da bilinir), aynı isimde
+           fakat farklı parametre listelerine sahip birden fazla metodu
+           bir sınıf içinde tanımlama yeteneğidir.
+  */
+
+}
---
+++ javademos/overloading/src/main/java/org/etiya/Main.java
@@ -0,0 +1,12 @@
+package org.etiya;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Calculation calculation = new Calculation();
+        System.out.println(calculation.total(2, 2));
+        System.out.println(calculation.total(2, 2, 2));
+
+
+    }
+}
