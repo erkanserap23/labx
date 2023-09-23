57cd85cbaa6be598a2a5aa5f3d067fd2575ec30d
serap.erkan <serap.erkannn@gmail.com>
2023-09-24 02:10:35 +0300

    camp lesson 1

--- chapter_1/src/main/java/org/example/Main.java
+++ chapter_1/src/main/java/org/example/Main.java
@@ -1,8 +1,38 @@
 package org.example;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hello and welcome!");
+        String middleText = "It may interest you";
+        String text = "Term time";
+
+        System.out.println(middleText);
+        int maturity = 12;
+        double dollarYesterday = 18.14;
+        double dollarToday = 18.10;
+
+        String arrowDirection = "";
+        if (dollarToday < dollarYesterday) {
+            arrowDirection = "down.svg";
+            System.out.println(arrowDirection);
+        } else if (dollarToday > dollarYesterday) {
+            arrowDirection = "up.svg";
+            System.out.println(arrowDirection);
+        } else {
+            arrowDirection = ("equal.svg");
+            System.out.println(arrowDirection);
+        }
+
+        //array
+        String[] credit = {"halk bank", "vakıf bank", "..."};
+        System.out.println(credit[0]);
+        System.out.println(credit[1]);
+        System.out.println(credit[2]);
+
+        for (int i = 0; i < credit.length; i++) {
+            System.out.println(credit[i]);
+        }
 
 
     }
