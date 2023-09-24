aef36b966a34f6fc8a2814b7e01d46af421b4830
serap.erkan <serap.erkannn@gmail.com>
2023-09-24 14:17:32 +0300

    category ,course and teacher class added.

---
+++ chapter_3/training_3/src/main/java/oopWithNLayeredApp/entities/Teacher.java
@@ -0,0 +1,33 @@
+package oopWithNLayeredApp.entities;
+
+public class Teacher {
+
+     private  String name;
+     private  int id;
+
+     private  String mail;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
