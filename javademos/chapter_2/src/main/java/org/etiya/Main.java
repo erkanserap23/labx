fc8421531153811bd59cfb4bbc1907af8061db26
serap.erkan <serap.erkannn@gmail.com>
2023-09-24 07:18:51 +0300

    added properties to the product class and created an object of the product class

---
+++ javademos/chapter_2/src/main/java/org/etiya/Main.java
@@ -0,0 +1,45 @@
+package org.etiya;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Product product1 = new Product();
+
+        product1.name = "Delonghi Kahve makinası";
+        product1.unitPrice = 7500;
+        product1.discount = 7;
+        product1.unitsInStock = 3;
+        product1.imageUrl = "eeff.jpg";
+
+        System.out.println(product1.name);
+
+        Product product2 = new Product();
+        product2.name = "Smeg Kahve makinası";
+        product2.unitPrice = 7500;
+        product2.discount = 7;
+        product2.unitsInStock = 3;
+        product2.imageUrl = "eeff.jpg";
+
+        System.out.println(product2.name);
+
+
+        Product product3 = new Product();
+        product3.name = "Kitchen Kahve makinası";
+        product3.unitPrice = 7500;
+        product3.discount = 7;
+        product3.unitsInStock = 3;
+        product3.imageUrl = "eeff.jpg";
+
+        System.out.println(product3.name);
+        System.out.println("-----------");
+
+        Product[] products = {product1, product2, product3};
+
+
+        for (Product product : products) {
+            System.out.println(product.name);
+        }
+    }
+}
+
---
+++ javademos/chapter_2/src/main/java/org/etiya/Product.java
@@ -0,0 +1,17 @@
+package org.etiya;
+
+public class Product {
+    String name;
+    double unitPrice;
+    double discount;
+    String imageUrl;
+    int unitsInStock;
+
+}
+
+
+//  !Pascal case kelimenin ilk harfi büyük
+//  oop nesne güdümlü programala dilidir.
+//  nesne yönelimli - nesne güdümlü programlar sürdürebilirlik katar.
+//  deger atama set - deger okuma get
+//  Java'da "class" (sınıf), nesne yönelimli programlamada (OOP) temel bir yapı taşıdır.
