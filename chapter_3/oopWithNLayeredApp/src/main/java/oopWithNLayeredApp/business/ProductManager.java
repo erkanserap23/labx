d9b42b9c922bf37b344a6087b7399bc2e6bcdc1e
serap.erkan <serap.erkannn@gmail.com>
2023-09-24 11:54:46 +0300

    interface polymorphism - dependency injection

--- chapter_3/oopWithNLayeredApp/src/main/java/oopWithNLayeredApp/business/ProductManager.java
+++ chapter_3/oopWithNLayeredApp/src/main/java/oopWithNLayeredApp/business/ProductManager.java
@@ -1,12 +1,18 @@
 package oopWithNLayeredApp.business;
+import oopWithNLayeredApp.core.logging.Logger;
 import oopWithNLayeredApp.dataAccess.ProductDao;
 import oopWithNLayeredApp.entities.Product;
+
+import java.util.List;
 
 
 public class ProductManager {
     private  ProductDao productDao;
-    public ProductManager(ProductDao productDao) {
+    private Logger[] loggers;
+    // private Logger[] loggers;
+    public ProductManager(ProductDao productDao,Logger[] loggers) {
         this.productDao = productDao;
+        this.loggers=loggers;
     }
 
 
@@ -16,6 +22,12 @@ public class ProductManager {
             throw  new Exception("Ürün fiyatı 10dan küçüktür.");
         }
         productDao.add(product);
+     // logerları çalıştıralım kaçtaneyse
+
+        for(Logger logger:loggers){
+            logger.log(product.getName());
+        }
+
 
         //JdbcProductDao productDao= new JdbcProductDao();
         //productDao.add(product);
