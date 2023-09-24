818e543242419884f32e3936055751f1137278fb
serap.erkan <serap.erkannn@gmail.com>
2023-09-24 14:49:00 +0300

    dataAccess layer added

---
+++ chapter_3/training_3/src/main/java/oopWithNLayeredApp/dataAccess/HibernateDao.java
@@ -0,0 +1,22 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Category;
+import oopWithNLayeredApp.entities.Course;
+import oopWithNLayeredApp.entities.Teacher;
+
+public class HibernateDao implements TeacherDao,CategoryDao, CourseDao {
+    @Override
+    public void add(Category category) {
+
+    }
+
+    @Override
+    public void add(Course course) {
+
+    }
+
+    @Override
+    public void add(Teacher teacher) {
+
+    }
+}
---
+++ chapter_3/training_3/src/main/java/oopWithNLayeredApp/dataAccess/TeacherDao.java
@@ -0,0 +1,7 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Teacher;
+
+public interface TeacherDao {
+    void add(Teacher teacher);
+}
