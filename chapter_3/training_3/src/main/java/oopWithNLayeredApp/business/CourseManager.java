df70c38851ad3bbe8f2ff0c5e1cb6ea8e34de219
serap.erkan <serap.erkannn@gmail.com>
2023-09-24 15:02:27 +0300

    courseManager added

---
+++ chapter_3/training_3/src/main/java/oopWithNLayeredApp/business/CourseManager.java
@@ -0,0 +1,26 @@
+package oopWithNLayeredApp.business;
+
+import oopWithNLayeredApp.dataAccess.CourseDao;
+import oopWithNLayeredApp.entities.Course;
+
+public class CourseManager {
+    private CourseDao courseDao;
+    public CourseManager(CourseDao courseDao) {
+        this.courseDao = courseDao;
+    }
+
+
+    public void add(Course curse) throws Exception {
+
+        if(curse.getUnitPrice()<0){
+            throw new Exception("The product price is less than 0.");
+        }
+        courseDao.add(curse);
+    }
+
+
+
+
+
+}
+
--- chapter_3/training_3/src/main/java/oopWithNLayeredApp/entities/Course.java
+++ chapter_3/training_3/src/main/java/oopWithNLayeredApp/entities/Course.java
@@ -5,6 +5,8 @@ public class Course {
     private String name;
     private int id;
 
+    private int unitPrice;
+
 
     public String getName() {
         return name;
@@ -20,5 +22,13 @@ public class Course {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(int unitPrice) {
+        this.unitPrice = unitPrice;
     }
 }
